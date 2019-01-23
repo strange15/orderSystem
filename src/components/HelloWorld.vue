fe2ace20165bb8fc5fbaee4a0ec971f98a0cdd2a
Strange Wang <strange66666666@gmail.com>
2019-01-23 17:21:42 +0800

    build UI with bootstrap

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,451 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-6">
+                <h3>前菜</h3>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/output/food-q-c-64-64-8.jpg"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">龍蝦沙拉</h5>不含龍蝦的沙拉
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="appetizerRadios"
+                          id="appetizerRadios1"
+                          value="option1"
+                          checked
+                        >
+                        <label class="form-check-label" for="appetizerRadios1">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/output/city-q-c-64-64-3.jpg"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">和風生牛肉</h5>生食
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="appetizerRadios"
+                          id="appetizerRadios2"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="appetizerRadios2">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/output/nightlife-q-c-64-64-9.jpg"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">煙燻鮭魚</h5>冷盤
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="appetizerRadios"
+                          id="appetizerRadios3"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="appetizerRadios3">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+                <h3>甜點</h3>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/output/abstract-q-g-64-64-1.jpg"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">巧克力布朗尼</h5>巧克力
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="dessertRadios"
+                          id="dessertRadios1"
+                          value="option1"
+                          checked
+                        >
+                        <label class="form-check-label" for="dessertRadios1">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/output/animals-q-g-64-64-3.jpg"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">巧克力熔岩冰淇淋</h5>冰與火之歌
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="dessertRadios"
+                          id="dessertRadios2"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="dessertRadios2">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/output/business-q-g-64-64-9.jpg"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">草莓歐菲香</h5>甜甜圈
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="dessertRadios"
+                          id="dessertRadios3"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="dessertRadios3">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-6">
+                <h3>湯</h3>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/1"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">羅宋湯</h5>青菜蘿蔔
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="soupRadios"
+                          id="soupRadios1"
+                          value="option1"
+                          checked
+                        >
+                        <label class="form-check-label" for="soupRadios1">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/2"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">玉米濃湯</h5>濃醇香
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="soupRadios"
+                          id="soupRadios2"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="soupRadios2">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/3"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">洋蔥湯</h5>內含牛肉
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="soupRadios"
+                          id="soupRadios3"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="soupRadios3">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+                <h3>飲料</h3>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/4"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">美式咖啡</h5>黑咖啡
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="drinkRadios"
+                          id="drinkRadios1"
+                          value="option1"
+                          checked
+                        >
+                        <label class="form-check-label" for="drinkRadios1">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/5"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">拿鐵咖啡</h5>咖啡+牛奶
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="drinkRadios"
+                          id="drinkRadios2"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="drinkRadios2">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/6"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">柳橙汁</h5>小朋友注意不要跌倒
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="drinkRadios"
+                          id="drinkRadios3"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="drinkRadios3">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-6">
+                <h3>主菜</h3>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/7"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">波士頓龍蝦</h5>整隻給你吃
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="mainRadios"
+                          id="mainRadios1"
+                          value="option1"
+                          checked
+                        >
+                        <label class="form-check-label" for="mainRadios1">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/8"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">德國豬腳</h5>台灣豬隻
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="mainRadios"
+                          id="mainRadios2"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="mainRadios2">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content">
+                    <div class="col-md-9">
+                      <div class="media">
+                        <img
+                          class="mr-3"
+                          alt="Bootstrap Media Preview"
+                          src="http://lorempixel.com/64/64/food/9"
+                        >
+                        <div class="media-body">
+                          <h5 class="mt-0">原燒和牛</h5>和牛!
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="mainRadios"
+                          id="mainRadios3"
+                          value="option2"
+                        >
+                        <label class="form-check-label" for="mainRadios3">choose</label>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
